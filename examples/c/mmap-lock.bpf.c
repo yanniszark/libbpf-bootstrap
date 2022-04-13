ef18640171c0f842aa853ad4588e5233beb18f4a
Ashfaqur Rahaman <ashfaq@cs.utah.edu>
2022-04-13 08:25:41 -0600

    Added spinlock implementation with shared memory

---
+++ examples/c/mmap-lock.bpf.c
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (c) 2019 Facebook
+
+#include <linux/bpf.h>
+#include <stdint.h>
+#include <bpf/bpf_helpers.h>
+
+char _license[] SEC("license") = "GPL";
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(map_flags, BPF_F_MMAPABLE);
+	__type(key, __u32);
+	__type(value, __u64);
+} data_map SEC(".maps");
+
+int my_pid = 0;
+
+SEC("raw_tracepoint/sys_enter")
+int test_mmap(void *ctx)
+{
+    int zero = 0, one = 1, two = 2, three = 3, four = 4;
+	__u64 val, *lock, *x, *y, *user_count, *kern_count;
+	
+    /* Only procced when syscall comes from the userspace prog */
+    int pid = bpf_get_current_pid_tgid() >> 32;
+    if (pid != my_pid)
+        return 0;
+
+	/* data_map[0] = data_map[0] * 2; */
+	lock = bpf_map_lookup_elem(&data_map, &zero);
+	x = bpf_map_lookup_elem(&data_map, &one);
+	y = bpf_map_lookup_elem(&data_map, &two);
+	user_count = bpf_map_lookup_elem(&data_map, &three);
+	kern_count = bpf_map_lookup_elem(&data_map, &four);
+	
+    if (!__sync_bool_compare_and_swap(lock, 0, 1)) {
+        return 0;
+    }
+    else {
+        (*x)++;     
+        (*y)++;     
+        if (*x != *y)
+            bpf_printk("data race!\n");
+        (*kern_count)++;
+        __sync_fetch_and_add(lock, 1);
+    }
+
+	return 0;
+}
+
---
+++ examples/c/mmap-lock.c
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <stdio.h>
+#include <unistd.h>
+#include <stdatomic.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include "mmap.skel.h"
+
+struct map_data {
+	__u64 val[512 * 4];
+};
+
+static size_t roundup_page(size_t sz)
+{
+	long page_size = sysconf(_SC_PAGE_SIZE);
+	return (sz + page_size - 1) / page_size * page_size;
+}
+
+int main(int argc, char *argv[])
+{
+	const size_t map_sz = roundup_page(sizeof(struct map_data));
+	const int zero = 0;
+	const long page_size = sysconf(_SC_PAGE_SIZE);
+	int err, data_map_fd, data_map_id;
+	struct bpf_map *data_map;
+	void *map_mmaped = NULL;
+	struct bpf_map_info map_info;
+	__u32 map_info_sz = sizeof(map_info);
+	struct map_data *map_data;
+	struct mmap_bpf *skel;
+    
+    atomic_uintmax_t *lock;
+	__u64 *x, *y, *user_count, *kern_count;
+
+	skel = mmap-lock_bpf__open();
+    if (!skel) {
+        fprintf(stderr, "skel_open: skeleton open failed!\n");
+        return;
+    }
+
+	/* at least 4 pages of data */
+	err = bpf_map__set_max_entries(skel->maps.data_map,
+				       4 * (page_size / sizeof(__u64)));
+
+    if (err) {
+        fprintf(stderr, "bpf_map__set_max_entries: failed!\n"); 
+        goto cleanup;
+    }
+	
+    /* ensure BPF program only handles syscalls from our process */
+	skel->bss->my_pid = getpid();
+
+	err = mmap-lock_bpf__load(skel);
+    if (err) {
+        fprintf(stderr, "skel_load: skeleton load failed!\n"); 
+        goto cleanup;
+    }
+
+	data_map = skel->maps.data_map;
+	data_map_fd = bpf_map__fd(data_map);
+
+	/* get map's ID */
+	memset(&map_info, 0, map_info_sz);
+	err = bpf_obj_get_info_by_fd(data_map_fd, &map_info, &map_info_sz);
+    if (err) {
+        fprintf(stderr, "map_get_info: failed %d\n", errno); 
+        goto cleanup;
+    }
+	data_map_id = map_info.id;
+
+	/* map as R/W first */
+	map_mmaped = mmap(NULL, map_sz, PROT_READ | PROT_WRITE, MAP_SHARED,
+			  data_map_fd, 0);
+    if (map_mmaped == MAP_FAILED) {
+        fprintf(stderr, "data_mmap: data_map mmap failed %d\n", errno); 
+        map_mmaped = NULL;
+        goto cleanup;
+    }
+	
+	map_data = map_mmaped;
+
+	err = mmap-lock_bpf__attach(skel);
+    if (err) {
+        fprintf(stderr, "attach_raw_tp: failed %d\n", err); 
+        goto cleanup;
+    }
+
+    /* Implement all synchronization */
+    lock = map_data->val;
+    x = &map_data->val + 1;
+    y = &map_data->val + 2;
+    user_count = &map_data->val + 3;
+    kern_count = &map_data->val + 4;
+
+    uintmax_t expected = 0;
+    while (1) {
+        while (!atomic_compare_exchange_strong(lock, &expected, 1))
+            expected = 0;
+        
+        (*x)++;
+        (*y)++;
+        
+        if (*x != *y) {
+            printf("data race!\n");
+            break;
+        }
+
+        (*user_count)++;
+        atomic_store(lock, 0);
+    }
+
+
+	mmap-lock_bpf__destroy(skel);
+	skel = NULL;
+	map_mmaped = NULL;
+
+	/* map should be still held by active mmap */
+	data_map_fd = bpf_map_get_fd_by_id(data_map_id);
+    if (data_map_fd < 0) {
+        fprintf(stderr, "get_map_by_id: failed %d\n", errno); 
+        munmap(map_mmaped, map_sz);
+        goto cleanup;
+    }
+	close(data_map_fd);
+
+	/* this should release data map finally */
+	munmap(map_mmaped, map_sz);
+
+	/* we need to wait for RCU grace period */
+	for (i = 0; i < 10000; i++) {
+		__u32 id = data_map_id - 1;
+		if (bpf_map_get_next_id(id, &id) || id > data_map_id)
+			break;
+		usleep(1);
+	}
+
+cleanup:
+	if (map_mmaped)
+		munmap(map_mmaped, map_sz);
+	mmap-lock_bpf__destroy(skel);
+}
